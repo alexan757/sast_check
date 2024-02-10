bcc624b42a30c8023aa7b70d6c4298db254bf4da
alexan757 <127663794+alexan757@users.noreply.github.com>
2024-02-10 20:54:35 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -4,9 +4,6 @@ template_vars['output'] = commands.getstatusoutput('/usr/bin/process_soemthing')
 def find_user(username):
     TWITTER_OAUTH_TOKEN = "dkedjekdjekldjekldje"
     with connection.cursor() as cur:
-
         cur.execute(f"""select username from USERS where name = '%s'""" % username)
-
         output = cur.fetchone()
-
     return output
