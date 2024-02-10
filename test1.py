8494b23fbcc833525abce2629f23068493ee9e9d
alexan757 <127663794+alexan757@users.noreply.github.com>
2024-02-10 20:19:41 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,11 +1,22 @@
-from django.db import connection
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'admin';
 
-def find_user(username):
+DROP TABLE IF EXISTS `account`;
 
-    with connection.cursor() as cur:
+CREATE TABLE `account` (
+  `name` varchar(60) DEFAULT NULL,
+  `userid` varchar(60) NOT NULL,
+  `password` varchar(60) DEFAULT NULL,
+  `balance` int(11) DEFAULT NULL,
+  `email` varchar(60) DEFAULT NULL,
+  `contactno` int(11) DEFAULT NULL,
+  PRIMARY KEY (`userid`)
+);
 
-        cur.execute(f"""select username from USERS where name = '%s'""" % username)
+LOCK TABLES `account` WRITE;
 
-        output = cur.fetchone()
+INSERT INTO `account` VALUES
+ ('Yakuza','aa','aa',54725,'yakuzarockz@japan.com',9843059),
+ ('Bogambo Vila','bagambo','bogambo',24350,'bogambo@paki.com',4643614),
+ ('Heth Ledger','heth1','heth1',20000,'heth@gotham.com',6434614);
 
-    return output
+UNLOCK TABLES;
